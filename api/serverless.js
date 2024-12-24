ca038dc5f15c5b147212faba13628f61f84e5cf7
Ectsang <1057844+Ectsang@users.noreply.github.com>
2024-12-24 02:47:13 +0000

    use fastify serverless example

---
+++ api/serverless.js
@@ -0,0 +1,22 @@
+"use strict";
+
+import * as dotenv from "dotenv";
+dotenv.config();
+
+// Require the framework
+import Fastify from "fastify";
+
+// Instantiate Fastify with some config
+const app = Fastify({
+  logger: true,
+});
+
+// Register your application as a normal plugin.
+app.register(import("../src/index"), {
+  prefix: "/",
+});
+
+export default async (req, res) => {
+  await app.ready();
+  app.server.emit("request", req, res);
+};
--- src/index.ts
+++ src/index.ts
@@ -10,46 +10,6 @@ import {
 } from "./helpers/verifyingPaymasters";
 import { createRpcHandler, createSbcRpcHandler } from "./relay";
 
-// const main = async () => {
-//   const walletClient = await getAnvilWalletClient();
-//   const verifyingPaymasterV07 = await setupVerifyingPaymasterV07(walletClient);
-//   const verifyingPaymasterV06 = await setupVerifyingPaymasterV06(walletClient);
-
-//   const altoBundlerV07 = createPimlicoBundlerClient({
-//     chain: await getChain(),
-//     transport: http(process.env.ALTO_RPC),
-//     entryPoint: ENTRYPOINT_ADDRESS_V07,
-//   });
-
-//   const altoBundlerV06 = createPimlicoBundlerClient({
-//     chain: await getChain(),
-//     transport: http(process.env.ALTO_RPC),
-//     entryPoint: ENTRYPOINT_ADDRESS_V06,
-//   });
-
-//   const app = Fastify({});
-
-//   app.register(cors, {
-//     origin: "*",
-//     methods: ["POST", "GET", "OPTIONS"],
-//   });
-
-//   const rpcHandler = createRpcHandler(
-//     altoBundlerV07,
-//     altoBundlerV06,
-//     verifyingPaymasterV07,
-//     verifyingPaymasterV06,
-//     walletClient
-//   );
-//   app.post("/", {}, rpcHandler);
-
-//   app.get("/ping", async (_request, reply) => {
-//     return reply.code(200).send({ message: "pong" });
-//   });
-
-//   await app.listen({ host: "0.0.0.0", port: 3000 });
-// };
-
 const main = async () => {
   const walletClient = getWalletClient();
 
